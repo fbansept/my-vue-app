6f972e377f7fda44da3c71a3bcde33943b3ee1d2
fbansept <bansept.franck@gmail.com>
2023-03-10 14:52:34 +0100

    test du dernier espoir de la fin

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,5 @@ export default defineConfig({
   base: "/my-vue-app/",
   build: {
     outDir: 'docs', // utilisez "docs" au lieu de "dist" car GitHub Pages utilise ce répertoire pour afficher votre site
-    lib: {
-      entry: 'src/my-element.js',
-      formats: ['es'],
-    },
-    rollupOptions: {
-      external: /^lit/,
-    },
   },
 })
