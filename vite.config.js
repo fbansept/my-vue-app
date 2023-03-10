b1130773aa1e739c2d4bf0fd76c7af23c5f3cce4
fbansept <bansept.franck@gmail.com>
2023-03-10 14:49:30 +0100

    encore un test :(

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   base: "/my-vue-app/",
   build: {
+    outDir: 'docs', // utilisez "docs" au lieu de "dist" car GitHub Pages utilise ce répertoire pour afficher votre site
     lib: {
       entry: 'src/my-element.js',
       formats: ['es'],
